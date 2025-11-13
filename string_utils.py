a36c12c089c90f1089b5e87f2b13d9ff2268d9ac
319129714 <roishvimer@gmail.com>
2025-11-13 13:39:28 +0200

    סופי

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,18 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
-
+    if not formula:
+        return []
+        
+    start = 0
+    split_formula = []
+    
+    for i in range(1, len(formula)):
+        if formula[i].isupper():
+            split_formula.append(formula[start:i])
+            start = i
+            
+    split_formula.append(formula[start:len(formula)])
+    
+    return split_formula
 
 def split_at_first_digit(formula):
     digit_location = 1
