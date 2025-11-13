0eeb3f13fe8cfd56ac016a14eb0e20e5ca69d3c0
319129714 <roishvimer@gmail.com>
2025-11-13 13:33:44 +0200

    משימה 2

--- string_utils.py
+++ string_utils.py
@@ -3,4 +3,20 @@ def split_before_each_uppercases(formula):
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    digit_location = 1
+    for char in formula[1:]:
+        if char.isdigit():
+            break
+        digit_location += 1
+    if digit_location == len(formula):
+        return formula, 1
+    else:
+        prefix = formula[:digit_location]
+        numeric_str = formula[digit_location:] 
+        numeric_int = 0
+        if numeric_str:
+            try:
+                numeric_int = int(numeric_str)
+            except ValueError:
+                pass
+        return prefix, numeric_int
